8b50eca46bc92126224a2a6f0486e46052656b45
陈礼安 <chenlian@chenliandeMacBook-Pro.local>
2020-12-22 11:56:08 +0800

    Initial commit

---
+++ src/cn/taiping/scala/HelloDemo.scala
@@ -0,0 +1,38 @@
+package cn.taiping.scala
+
+object HelloDemo {
+
+  def main(args: Array[String]): Unit = {
+  //  println("hello world !!!")
+
+    val a: Int => Int ={
+      b=> b* 10
+    }
+
+//    println(a(2));
+
+    def f(): Unit = {
+      println("function")
+    }
+
+
+    def f0()={
+      f _
+    }
+
+   //  f0()()
+
+    def f3(i:Int)(j:Int): Int = {
+      i * j
+    }
+
+    println(f3(2)(3))
+
+    def f4(f:()=> Int) :Unit = {
+      
+    }
+
+
+
+  }
+}
